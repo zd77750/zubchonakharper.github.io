09ed74c48da4e5339fa6229272e2b2b47ec15df0
Daniel Zubchonak <zd77750@mail.harpercollege.edu>
2026-09-01 15:54:55 -0500

    commit

--- web200/assignment2.js
+++ web200/assignment2.js
@@ -6,6 +6,6 @@ let studentFullName = studentfName + studentlName;
 
 document.getElementById("demo").innerHTML="This is" + studentFullName + "'s Assignment #2.";
 
-document.getElementById("intro").innerHTML= "This is" + studentFullName + "'s personal introduction to Assignment #2.";
+document.getElementById("intro").innerHTML= studentFullName + "'s personal introduction to Assignment #2.";
 
 
