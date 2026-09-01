07588800bdd81c5832916739d0a28697f7a359dd
Daniel Zubchonak <zd77750@mail.harpercollege.edu>
2026-09-01 17:32:44 -0500

    commit

--- web200/assignment2.js
+++ web200/assignment2.js
@@ -1,7 +1,24 @@
+/*
+"var" is used before ES6, a global scope that's easy to redeclarate any variable. Can get difficult to maintain; volatile to bugs and compromises.
+
+"let" is a block scope that prevents repetition of declarations within the scope, can be updated using reassignment. 
+
+"const" is another block scope, variable is set and cannot be updated by an end-user.
+ */
+
+// The "var" here is set to welcome users to the website (end-users can also redeclarate this var):
+ var welcomeClass = "You have entered WEB 200 at William Rainey Harper College"
+ var welcomeMetallica = "You have entered the Sandman by Metallica, Join Us live at the Las Vegas Sphere!"
+// This is me hacking into the mainframe >:-)
+ window.alert(welcomeMetallica)
+
+ // The "const" remains unchanged, JavaScript identifies the First and Last name of student. 
+ // The "let" is allowed to be used as a variable when combining "studentfName" and "studentlName". (must include + and " " to prevent concatenation)
 const studentfName = "Daniel";
 const studentlName = "Zubchonak";
 let studentFullName = studentfName + " " + studentlName;
 
+ // JavaScript Data can be displayed using possibile outputs like the one below (.innerHTML allows user to identify; "id", the element and write text.)
 document.getElementById("demo").innerHTML="This is " + studentFullName + "'s Assignment #2.";
 
 document.getElementById("intro").innerHTML= studentFullName + "'s personal introduction to Assignment #2.";
