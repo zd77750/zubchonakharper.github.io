784c5760be6eae3b012a85e11e230a9b78183052
Daniel Zubchonak <zd77750@mail.harpercollege.edu>
2026-09-12 14:27:28 -0500

    commit

--- web200/assignment3.js
+++ web200/assignment3.js
@@ -6,8 +6,8 @@ let Fahrenheit = F;
 
 
 if (Celsius < 16) {
-    document.getElementById("TempConverter").innerHTML = "The Tempature today in Las Vegas is " + Celsius + " degrees. Please put on Sunscreen today, it will be a Hot Day!";
+    document.getElementById("TempConverter").innerHTML = "The Tempature today in Las Vegas is " + Celsius + " degrees Celsius. Please put on Sunscreen today, it will be a Hot Day!";
 }
 if (Celsius > 16) {
-    document.getElementById("TempConverter").innerHTML = "The Tempature today in Las Vegas is " + Celsius + " degrees. It is expected to be lower than average, it will be a Chilly Day!";
+    document.getElementById("TempConverter").innerHTML = "The Tempature today in Las Vegas is " + Celsius + " degrees Celsius. It is expected to be lower than average, it will be a Chilly Day!";
 }
