0eb75c178a926a52822510b4687e94a1295c8b7a
Daniel Zubchonak <zd77750@mail.harpercollege.edu>
2026-09-17 10:23:45 -0500

    commit

--- web200/assignment4.js
+++ web200/assignment4.js
@@ -1,1 +1,30 @@
-function
+//Function #1
+//Basic Function allows JS to return numbers using "id" and "innerHTML"
+
+function AddFunction (num1, num2) {
+    return num1 + num2
+}
+
+document.getElementById("function1").innerHTML = AddFunction(5,8);
+
+
+
+//Function #2
+//**NOTICE** Semicolon (;) is including at the end of function ("{,}") due to "let" being present.
+
+let MultiplyFunction = function(num1,num2){
+    return num1 * num2;
+};
+
+document.getElementById("function2").innerHTML = MultiplyFunction(15,6)
+
+
+
+//Function #3
+//Using the ARROW Expression results having an arrow replacing traditional equals sign for operation.
+
+let SubtractFunction = function(num1,num2) => {
+    return num1 - num2;
+};
+
+document.getElementById("function3").innerHTML = SubtractFunction(95-86);
