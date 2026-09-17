05c798bebb6e500ceca0a5901b9d3905ef7c7992
Daniel Zubchonak <zd77750@mail.harpercollege.edu>
2026-09-17 11:20:58 -0500

    commit

--- web200/assignment4.js
+++ web200/assignment4.js
@@ -1,33 +1,33 @@
 //Function #1
 //Basic Function allows JS to return numbers using "id" and "innerHTML"
 
-function AddFunction (num1, num2) {
+function addFunction (num1, num2) {
     return num1 + num2
 }
 
-document.getElementById("function1").innerHTML = "The total of my Addition function is " + AddFunction(5,8);
+document.getElementById("function1").innerHTML = "The total of my Addition function is " + addFunction(5,8);
 
 
 
 //Function #2
 //**NOTICE** Semicolon (;) is including at the end of function ("{,}") due to "let" being present.
 
-let MultiplyFunction = function(num1,num2){
+let multiplyFunction = function(num1,num2){
     return num1 * num2;
 };
 
-document.getElementById("function2").innerHTML = "The total of my Multiplication function is " + MultiplyFunction(15,6)
+document.getElementById("function2").innerHTML = "The total of my Multiplication function is " + multiplyFunction(15,6)
 
 
 
 //Function #3
 //Using the ARROW Expression results having an "equal/arrow" replacing traditional "function/equals" sign for operation.
 
-let SubtractFunction = (num1,num2) => {
+let subtractFunction = (num1,num2) => {
     return num1 - num2;
 };
 
-document.getElementById("function3").innerHTML = "The total of my Subtraction function is " + SubtractFunction(95,86);
+document.getElementById("function3").innerHTML = "The total of my Subtraction function is " + subtractFunction(95,86);
 
 
 //**EXTRA CREDIT** - Custom Buttons
