5e7f560c7937b0311ad417dbbf10a5860a80eff8
Daniel Zubchonak <zd77750@mail.harpercollege.edu>
2026-09-22 12:41:01 -0500

    commit

--- web200/events.js
+++ web200/events.js
@@ -15,6 +15,7 @@ function keyPress(id) {
 }
 //Event Joke #4 - Page Load Event
 function pageLoad (id) {
-    document.getElementById("eventPageLoad").innerHTML = "PAGE LOADED: Welcome to my Assignment #5. This is a Page Load Event example.";
+    document.getElementById("eventPageLoad").innerHTML = 
+    '<img src="welcome-to-internet.webp" alt="Assignment #5">';
 }
 //*EXTRA CREDIT*
