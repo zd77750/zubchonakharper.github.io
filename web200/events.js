0f748225a8920d9f6e2a5aafb973a46d75aebaed
Daniel Zubchonak <zd77750@mail.harpercollege.edu>
2026-09-22 13:00:20 -0500

    commit

--- web200/events.js
+++ web200/events.js
@@ -1,21 +1,33 @@
 //Event Joke #1 - Click Event
+
 function changeText(id) {
     id.innerHTML = 
     "<b>I dislocated my shoulder at the gym while doing dumbbell overhead presses using 50lb dumbbells. <br> Daniel is a dumbbell himself...</b>";
 }
+
 //Event Joke #2 - Mouse Over Event
+
 function mouseOver(id) { 
     id.innerHTML = "<b>Jokes on you! The Mouse Over Event is the joke.</b>";
     id.style.color = "Red";
 }
+
+document.getElementById("eventJoke").addEventListener("mouseOver", function() {
+    mouseOver(this);
+})
+
 //Event Joke #3 - Keypress Event
+
 function keyPress(id) {
     id.value = 
     "You have pressed a button";
 }
+
 //Event Joke #4 - Page Load Event
+
 function pageLoad (id) {
     document.getElementById("eventPageLoad").innerHTML = 
     '<img src="web200/welcome-to-internet.webp" alt="Assignment #5">';
 }
+
 //*EXTRA CREDIT*
